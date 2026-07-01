e5d38fb05951d2dddb691c5ea325769f17226606
Jaya Kumar M <JayaKumar.M2@cognizant>
2026-07-01 15:41:39 +0530

    Changed file name

--- src/main/java/com/genc/hrms/repository/AttendanceRepository.java
+++ src/main/java/com/genc/hrms/repository/AttendanceRepository.java
@@ -1,4 +1,18 @@
 package com.genc.hrms.repository;
 
-public interface AttendanceRepository {
+import com.genc.hrms.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
+    Attendance save(Attendance attendance);
+
+    //    employee
+    List<Attendance> findByEmployee_EmployeeId(long id);
+
+    //  for manager
+    List<Attendance> findByStatus(Attendance.LeaveStatus status);
+
+    List<Attendance> findByLeaveTypeAndStatusAndEmployee_EmployeeId(Attendance.Leave leaveType, Attendance.LeaveStatus status, long id);
 }
