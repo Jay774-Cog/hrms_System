02dc26f19768616b53e6bb00af055c91655c64d8
benjamin-cognizant <Benjamin.Luke@Cognizant.com>
2026-07-01 15:21:43 +0530

    added employee repository

---
+++ src/main/java/com/genc/hrms/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.genc.hrms.repository;
+
+import com.hrms.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
+
